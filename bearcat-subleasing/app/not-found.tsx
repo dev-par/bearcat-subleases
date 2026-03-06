3daf98440b857b31acfa9c756475bb2fe1c83cd3
dev-par <146295367+dev-par@users.noreply.github.com>
2026-03-06 17:43:50 -0500

    Update 404 page

--- bearcat-subleasing/app/not-found.tsx
+++ bearcat-subleasing/app/not-found.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link'
  
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className='flex items-center justify-center h-screen'>
+      <div className='text-center'>
+        <h2 className='text-2xl font-bold md-2'>Not Found</h2>
+        <p className='md-4'>Could not find requested resource</p>
+        <Link href="/" className='mb-4 text-blue-600 hover:text-blue-700 hover:underline transition'>Return Home</Link>
+      </div>
     </div>
   )
 }
